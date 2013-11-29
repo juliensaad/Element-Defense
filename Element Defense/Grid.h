155f62413809eb134e71ed9bb05a203032bf69d0
saadjulien <jsaad@thirdbridge.net>
2013-11-29 10:52:11 -0500

    Added Grid

---
+++ Element Defense/Grid.h
@@ -0,0 +1,15 @@
+//
+//  Grid.h
+//  Element Defense
+//
+//  Created by Julien Saad on 11/29/2013.
+//  Copyright (c) 2013 Third Bridge. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Grid : NSObject
+
+@property (nonatomic) int height;
+@property (nonatomic) int width;
+@end
---
+++ Element Defense/Grid.m
@@ -0,0 +1,13 @@
+//
+//  Grid.m
+//  Element Defense
+//
+//  Created by Julien Saad on 11/29/2013.
+//  Copyright (c) 2013 Third Bridge. All rights reserved.
+//
+
+#import "Grid.h"
+
+@implementation Grid
+
+@end
